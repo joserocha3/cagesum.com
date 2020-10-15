768f08f55b2deb12e19a3eda41d478d45835aca7
Pablo Rocha <jose.rocha3@gmail.com>
2020-10-14 20:24:18 -0500

    correct tag manager code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,13 +11,16 @@ class Document extends NextDocument {
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'GTM-5FLCL8D');            
-                `,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5FLCL8D');`,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FLCL8D" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
         </Head>
