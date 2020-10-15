28791e3173ad5017b26e5bb61de9acd667abb777
Pablo Rocha <jose.rocha3@gmail.com>
2020-10-14 20:16:06 -0500

    install tag manager

---
+++ src/pages/_document.js
@@ -0,0 +1,33 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-5FLCL8D`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'GTM-5FLCL8D');            
+                `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
