dbea8e229bf0d7971f9a8a33a83765e385328538
Pablo Rocha <jose.rocha3@gmail.com>
2024-02-29 21:56:32 -0600

    add favicon

--- app.vue
+++ app.vue
@@ -77,6 +77,29 @@ const unlockTheCage = () => {
 </script>
 
 <template>
+  <Header>
+    <Link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/favicon/apple-touch-icon.png"
+    />
+    <Link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicon/favicon-32x32.png"
+    />
+    <Link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/favicon/favicon-16x16.png"
+    />
+    <Link rel="manifest" href="/favicon/site.webmanifest" />
+    <Meta name="msapplication-TileColor" content="#da532c" />
+    <Meta name="theme-color" content="#ffffff" />
+  </Header>
+
   <div class="bg-custom-gradient pt-4 pb-8 text-zinc-600">
     <header class="text-right max-w-[600px] m-auto pb-4">
       Made by
